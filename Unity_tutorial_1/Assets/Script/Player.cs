15598286725b4be0e313d05f73698f610fae10a3
yukiya-yokoi2 <okoeliuwei367@gmail.com>
2022-11-25 11:50:43 +0900

    Wrote a program to move a character

--- Unity_tutorial_1/Assets/Script/Player.cs
+++ Unity_tutorial_1/Assets/Script/Player.cs
@@ -4,34 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //インスペクターで設定する
+    public float speed;
 
+    //プライベート設定
     private Animator anim = null;
+    private Rigidbody2D rb = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        //コンポーネントのインスタンスを捕まえる
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        //キー入力されたら行動する
         float horizontalKey = Input.GetAxis("Horizontal");
+        float xSpeed = 0.0f;
+
 
         if (horizontalKey > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
             anim.SetBool("run", true);
+            xSpeed = speed;
         }
         else if(horizontalKey < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
             anim.SetBool("run", true);
+            xSpeed = -speed;
         }
         else
         {
             anim.SetBool("run", false);
+            xSpeed = 0.0f;
         }
+        rb.velocity = new Vector2(xSpeed, rb.velocity.y);
     }
 }
